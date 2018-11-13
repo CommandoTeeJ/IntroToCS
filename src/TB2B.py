d17a32a51bed7782e326e8adb74feffcb017b7cd
Tristan Jordan <35291790+CommandoTeeJ@users.noreply.github.com>
2018-11-13 15:46:20 -0500

    Create TB2B.py

---
+++ src/TB2B.py
@@ -0,0 +1,10 @@
+###Replace number beside tb###
+tb= 3
+###Do not replace past this point###
+gb= (tb*1024)
+mb= (gb*1024)
+kb= (mb*1024)
+bytes= (kb*1024)
+###Display###
+print ('bytes: ',bytes,' KB: ',kb,' MB: ',mb,' GB: ',gb,' TB: ',tb)
+###By Tristan Jordan###
