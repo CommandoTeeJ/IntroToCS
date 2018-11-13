1110a2757a91e5dbdc868ecd0eef766e87ad3c98
Tristan Jordan <35291790+CommandoTeeJ@users.noreply.github.com>
2018-11-13 15:43:50 -0500

    Create OldMacDonald.py

---
+++ src/OldMacDonald.py
@@ -0,0 +1,16 @@
+###Created by Tristan Jordan
+###Nov, 1
+###This program creates the lyrics to old macdonald
+def oldmac(ani,sound):
+    print("Old MacDonald had a farm, Ee-igh, Ee-igh, Oh!")
+    print("And on that farm he had a", ani ,", Ee-igh, Ee-igh, Oh!")
+    print("with a ", sound , ", ", sound , "here and a ", sound , ", " , sound , "there.")
+    print("Here a ", sound , ", there a ", sound , ", everywhere a ", sound , ", ", sound, ".")
+    print("Old MacDonald had a farm, Ee-igh, Ee-igh, Oh!")
+def main():
+    oldmac("pig","oink")
+    oldmac("cow","moo")
+    oldmac("horse","neigh")
+    oldmac("chicken","bawk")
+    oldmac("fish","blub")
+main()
